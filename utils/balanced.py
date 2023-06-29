4902717f17f10f120ef8210c3821390da12edd57
k6mui <106663422+k6mui@users.noreply.github.com>
2023-06-29 11:24:10 +0200

    Update balanced.py

--- utils/balanced.py
+++ utils/balanced.py
@@ -8,28 +8,30 @@ def load_and_combine_datasets(paths):
         df_total = pd.concat(dataframes)
         return df_total
     except FileNotFoundError as e:
-        print(f"Uno de los archivos no se encuentra en la ruta especificada: {e}")
+        print(f"One of the files is not found in the specified path: {e}")
         return None
 
 def split_dataset(df, label_column='Attack'):
     X = df.drop(label_column, axis=1)
     y = df[label_column]
 
-    # Divide el dataset en dos partes (66% para el conjunto temporal, 34% para el tercer conjunto)
+    # Split the dataset into two parts (66% for the temporal set, 34% for the third set)
     X_temp, X_third, y_temp, y_third = train_test_split(X, y, test_size=0.33, stratify=y, random_state=42)
 
-    # Divide el conjunto temporal en dos para crear los primeros dos conjuntos
+    # Split the temporal set into two to create the first two sets
     X_first, X_second, y_first, y_second = train_test_split(X_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=42)
 
     return pd.concat([X_first, y_first], axis=1), pd.concat([X_second, y_second], axis=1), pd.concat([X_third, y_third], axis=1)
 
 def save_datasets(datasets, paths):
     for df, path in zip(datasets, paths):
+        directory = os.path.dirname(path)
+        os.makedirs(directory, exist_ok=True)  # Create the directory if it doesn't exist
         df.to_csv(path, index=False)
 
 def print_label_distribution(datasets, names):
     for df, name in zip(datasets, names):
-        print(f"\nDistribución de etiquetas en el DataFrame {name}:\n")
+        print(f"\nLabel distribution in DataFrame {name}:\n")
         print(df['Attack'].value_counts().to_string())
 
 def balance(paths, save_paths, names):
@@ -43,5 +45,5 @@ def balance(paths, save_paths, names):
 if __name__ == "__main__":
     paths = ["../datasets/sampled/cicids_sampled.csv", "../datasets/sampled/nb15_sampled.csv", "../datasets/sampled/toniot_sampled.csv"]
     save_paths = ['../datasets/balanced/first_balanced.csv', '../datasets/balanced/second_balanced.csv', '../datasets/balanced/third_balanced.csv']
-    names = ["primero", "segundo", "tercero"]
+    names = ["first", "second", "third"]
     balance(paths, save_paths, names)
